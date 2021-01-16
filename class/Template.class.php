3ed794d45b5f835185c0be06457909cdf7740646
Gary11777 <iharushakou@gmail.com>
2021-01-16 17:00:32 +0300

    Added Template.class, template of main_menu and changed main template

---
+++ class/Template.class.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+class Template
+{
+    private $template;
+    private $placeholders = array();
+    private $labels = array();
+
+    public function __construct()
+    {
+
+    }
+
+
+}
